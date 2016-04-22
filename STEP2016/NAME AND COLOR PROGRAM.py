eb81b32f7eabf5836c4c3f6616c5193106623375
Evan Kapitansky <evankapitansky@gmail.com>
2016-04-22 11:11:48 -0400

    Created Temperature.py

--- STEP2016/NAME AND COLOR PROGRAM.py
+++ STEP2016/NAME AND COLOR PROGRAM.py
@@ -1,4 +1,5 @@
 
+'''
 def main():
     listprogram()
 
@@ -14,6 +15,35 @@ def listprogram():
             exit=True
     length=len(namelist)
     for a in range(0,length ):
-         print(namelist[a], colorlist[a])
+         print( namelist[a], colorlist[a])
+
+
+main()
+
+'''
+'''
+
+
+print("\033[1;37;40m Bright Colour\033[0;37;40m \n")
+'''
+
+def main():
+    listprogram()
+
+def listprogram():
+    newlist=[] # Will contain all names and colors
+
+    exit=False
+    while(exit==False):
+        newlist.append(input("What is your name? "))
+        newlist.append(input("What is your favorite color? "))
+        decision=input("Press q to quit. ")
+        if decision=="q" or decision=="Q":
+            exit=True
+    length=len(newlist)
+    for a in range(0,length,2):
+         print( newlist[a], end=" ")
+         print(newlist[a+1])
+
 
 main()
