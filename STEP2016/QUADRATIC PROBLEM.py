c5196ee6e4f590f6ae2a3c035fa6209d3c5b485a
Evan Kapitansky <evankapitansky@gmail.com>
2016-04-22 13:29:55 -0400

    Created Temperature.py

---
+++ STEP2016/QUADRATIC PROBLEM.py
@@ -0,0 +1,5 @@
+
+
+print(" "),print("    Quadratic equation: AX^2 + BX + C"),print(" ")
+a = float(input('Enter a: ')); b = float(input('Enter b: ')); c = float(input('Enter c: ')); d = (b**2) - (4*a*c)
+print(d)
