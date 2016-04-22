368cc42807e3155a8ee735ab46cb8825c71cee68
Evan Kapitansky <evankapitansky@gmail.com>
2016-04-22 14:21:35 -0400

    Created Temperature.py

--- STEP2016/QUADRATIC PROBLEM.py
+++ STEP2016/QUADRATIC PROBLEM.py
@@ -1,5 +1,45 @@
 
 
-print(" "),print("    Quadratic equation: AX^2 + BX + C"),print(" ")
-a = float(input('Enter a: ')); b = float(input('Enter b: ')); c = float(input('Enter c: ')); d = (b**2) - (4*a*c)
-print(d)
+print(" "), print("\033[5;37;40m Quadratic Equation AX^2 + BX + C\033[0;37;40m\n"), print(" ")
+
+ValidInput=False
+while(ValidInput==False):
+    try:
+        a = float(input('Enter A: '))
+        ValidInput=True
+    except:
+      print("Please enter a number.")
+
+ValidInput=False
+while(ValidInput==False):
+    try:
+        b = float(input('Enter B: '))
+        ValidInput=True
+    except:
+      print("Please enter a number.")
+
+ValidInput=False
+while(ValidInput==False):
+    try:
+        c = float(input('Enter C: '))
+        ValidInput=True
+    except:
+      print("Please enter a number.")
+
+ValidInput=False
+while(ValidInput==False):
+    try:
+        x = float(input('Enter X: '))
+        ValidInput=True
+    except:
+      print("Please enter a number.")
+
+d = (a*x**2)+(b*x)+c
+print("Your answer is: ",d)
+
+
+#print("\033[1;37;40m Quadratic Equation: AX^2 + BX + C\033[0;37;40m \n")
+print("\033[5;37;40m Quadratic Equation Solved\033[0;37;40m\n")
+
+print("\033[1;31;40m Quadratic Equation: AX^2 + BX + C \033")
+#print("\033[1;37;40m Quadratic Equation: AX^2 + BX + C\033[0;37;40m \n")
